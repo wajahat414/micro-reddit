8b637bb84e3220fe2f07b603f0e28f0b8dcc9745
wajahat414 <wajahatakram414@gmail.com>
2021-09-29 00:10:04 +0500

    adding comments association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
     validates :title,:link, presence: true
     validates :title,uniqueness: true
     belongs_to :user
+    has_many :comments
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     validates :password, length: { in: 6..20 }
     validates :age, numericality: { only_integer: true }
     has_many :posts, dependent: :destroy
-
+    has_many :comments, dependent: :destroy
+    
 end
