bdc3a19a4936e81249199d9df4b353ac22a55079
wajahat414 <wajahatakram414@gmail.com>
2021-09-29 00:36:38 +0500

    fixing style

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    validates :description,:user_id,:post_id,presence: true
+  belongs_to :user
+  belongs_to :post
+  validates :description, :user_id, :post_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
-class User < ApplicationRecord 
-    validates :name,:email,:password,:age, presence: true
-    validates :email,uniqueness: true
-    validates :password, length: { in: 6..20 }
-    validates :age, numericality: { only_integer: true }
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    
+class User < ApplicationRecord
+  validates :name, :email, :password, :age, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :age, numericality: { only_integer: true }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
